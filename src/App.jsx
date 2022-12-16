a401474e118ece1beb3360c4764e02cb30b00745
pazIago <iagopaz@hotmail.com.br>
2022-12-16 09:23:18 -0300

    deploy fix

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import Login from "./pages/Login/Login";
 import User from "./pages/User/User";
 import Photo from "./components/Photo/Photo";
 import UserProfile from "./pages/User/UserProfile";
-import NotFound from "./pages/NotFound.jsx";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
